e594ecb68291a9fa48966093e36d6c347972958f
Pizzaold <mihhailovjurmo@gmail.com>
2023-02-28 13:23:22 +0200

    Me as user get the order value with tax [Delivers #184526152]

--- ostu_calculator.py
+++ ostu_calculator.py
@@ -1,6 +1,7 @@
 kogus = input("Sisesta toode kogus: ")
 hind = input("Sisesta toote hind: ")
+Estonian_sales_tax = 0.2 # 20% on Eesti käibemaks
 
 print("Kokkus: " + kogus)
 print("Hind: " + hind)
-print("Kokku: " + str(int(kogus) * int(hind)))
+print("Kokku: " + str(int(kogus) * int(hind) * (1 + Estonian_sales_tax)))
